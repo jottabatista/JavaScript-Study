2e1a1bb5edbb087cd9c329bc7b32e1abbbc2f42e
João Guilherme Batista <batistadm01@gmail.com>
2021-08-20 19:55:17 -0300

    aula14-ex-02 is about parseInt, getElementById, for, let, console.log and placeholder.

--- aula14ex-02/script.js
+++ aula14ex-02/script.js
@@ -4,16 +4,6 @@ function tabuada(){
     //var num = 2;
     var tabuada = '';
     var resultInPoint = 0;
-
-    var variavel = "";
-    var variavel1 = true; // booleano, true or false
-    var variavel2 = 0;
-    var variavel3 = 1.2; // float = não inteiro E Z
-    // O nome da variavel pode ser o que vc quiser.
-    var bunda = 0;
-    var index123guilherme = 0;
-    var count = 0; //Contar
-
     
     var resultString = "";
     for (let index = 1; index <= 10; index++) {
@@ -23,7 +13,6 @@ function tabuada(){
         console.log(num + " x " + index + " = " + resultInPoint);
         resultString += `${num} x ${index} = ${resultInPoint} <br>`;
     }
-    //console.log(`Teste`);
     //resposta.innerHTML = tabuada;
     result.innerHTML = resultString;
 }
